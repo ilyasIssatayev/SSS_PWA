74e288b66674e4c8d7206bed50ba5c68f97d5bb5
Ilyas <issatayev.ilyas@gmail.com>
2020-11-17 15:23:48 +0100

    login now connected to Backend

--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
-
+import { WebService } from '../../services/web.service';
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
@@ -11,6 +11,9 @@ export class LoginPageComponent implements OnInit {
 
   email = new FormControl('', [Validators.required, Validators.email]);
 
+  account;
+  password;
+
   getErrorMessage() {
     if (this.email.hasError('required')) {
       return 'You must enter a value';
@@ -19,9 +22,35 @@ export class LoginPageComponent implements OnInit {
     return this.email.hasError('email') ? 'Not a valid email' : '';
   }
 
-  constructor() { }
+  constructor(private dataService: WebService)
+  {
+
+  }
 
   ngOnInit() {
+
+  }
+
+  updateAccount(event: any)
+  {
+    this.account = event.target.value;
+    console.log("account: ",this.account)
+  }
+
+  updatePassword(event: any)
+  {
+    this.password = event.target.value;
+    console.log("password: ",this.password )
+  }
+
+  onLogin(){
+    console.log("Login",{account:this.account,password:this.password} );
+    this.dataService.postLogin({account:this.account,password:this.password}).subscribe(
+      token => {
+        console.log("Subscribe")
+        console.log(token);
+      }
+  );;
   }
 
 }
---
+++ src/app/services/web.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { retry, catchError, map } from "rxjs/operators";
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class WebService {
+  url = "https://sedcon-backend.herokuapp.com";
+
+  constructor(private http: HttpClient) {
+
+  }
+
+    // Http Options
+    httpOptions = {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json"
+      })
+    };
+
+    postLogin(loginData): Observable<any> {
+      //console.log("postLogin: ",loginData);
+
+      let bodyToSend;
+
+      bodyToSend = {
+        "email":loginData.account,
+        "password":loginData.password
+      }
+      console.log("Post: Login with loginData",bodyToSend);
+      return this.http
+        .post<any>(this.url + "/login",bodyToSend ,this.httpOptions);
+}
+}
