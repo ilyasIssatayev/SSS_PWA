6aa9f44a976a77ecd79cfd2ae557ae77f944751e
Ilyas <issatayev.ilyas@gmail.com>
2021-01-15 14:19:12 +0100

    sss

--- src/app/components/profile-page/profile-page.component.ts
+++ src/app/components/profile-page/profile-page.component.ts
@@ -31,7 +31,7 @@ export class ProfilePageComponent implements OnInit {
 
   ngOnInit() {
     this.updateUserData();
-    this.dataService.updateProfile=this.updateUserData;
+    this.dataService.updateProfile=this;
   }
 
   goToEditInfo()
